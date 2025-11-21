4d01c862b263f00f2ee53a1272d0a3d3f9260000
felicia-yan <feliciay@stanford.edu>
2025-11-20 22:03:07 -0800

    small ui changes after demo

--- Assets/Scripts/CharacterCanvas.cs
+++ Assets/Scripts/CharacterCanvas.cs
@@ -22,7 +22,6 @@ public class CharacterCanvas : MonoBehaviour
         "Thinking...",
         "Pondering...",
         "Contemplating...",
-        "Processing...",
         "Reflecting...",
     };
 
